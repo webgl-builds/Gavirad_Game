114302ced4a1f54e97257a2ea8235be4cbe2c4b8
webgl-builds <dev_lufias@icloud.com>
2025-12-05 22:20:11 +0600

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,30 +1,35 @@
 const cacheName = "DefaultCompany-Gavirad Game-1.2";
 const contentToCache = [
-    "Build/Build_Gavirad.loader.js",
-    "Build/Build_Gavirad.framework.js",
-    "Build/Build_Gavirad.data",
-    "Build/Build_Gavirad.wasm",
-    "TemplateData/style.css"
-
+  "Build/Build_Gavirad.loader.js",
+  "Build/Build_Gavirad.framework.js",
+  "Build/Build_Gavirad.data",
+  "Build/Build_Gavirad.wasm",
+  "TemplateData/style.css"
 ];
 
-self.addEventListener('install', function (e) {
-    console.log('[Service Worker] Install');
-    
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(contentToCache);
+  })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
+self.addEventListener("activate", event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(
+      keys.map(key => {
+        if (key !== cacheName) return caches.delete(key);
+      })
+    );
+    await self.clients.claim();
+  })());
+});
 
-      response = await fetch(e.request);      
-      return response;
-    })());
+self.addEventListener("fetch", event => {
+  event.respondWith((async () => {
+    const cached = await caches.match(event.request);
+    return cached || fetch(event.request);
+  })());
 });
