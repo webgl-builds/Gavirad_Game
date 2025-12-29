17541b20998e0dc62fd9089c579d713b69918b47
Saiful Islam <saifulanik@icloud.com>
2025-12-29 22:35:22 +0600

    v1.3

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,10 +1,11 @@
-const cacheName = "DefaultCompany-Gavirad Game-1.2";
+const cacheName = "DefaultCompany-Gavirad Game-1.3";
 const contentToCache = [
-  "Build/Build_Gavirad.loader.js",
-  "Build/Build_Gavirad.framework.js",
-  "Build/Build_Gavirad.data",
-  "Build/Build_Gavirad.wasm",
-  "TemplateData/style.css"
+    "Build/build.loader.js",
+    "Build/build.framework.js",
+    "Build/build.data",
+    "Build/build.wasm",
+    "TemplateData/style.css"
+
 ];
 
 self.addEventListener("install", event => {
